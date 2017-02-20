143b385784eb40d1f9ce515f4b24171eb02d1483
xuhuafu <xuhuafu@shuame.com>
2017-02-20 16:17:13 +0800

    notification

--- notifaction/src/main/java/com/gmail/huashadow/study/notifaction/MainActivity.java
+++ notifaction/src/main/java/com/gmail/huashadow/study/notifaction/MainActivity.java
@@ -14,10 +14,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showCustomNotification();
         showNotification();
     }
 
-    private void showNotification() {
+    private void showCustomNotification() {
         Notification foregroundNote;
 
         RemoteViews bigView = new RemoteViews(getApplicationContext().getPackageName(),
@@ -28,9 +29,9 @@ public class MainActivity extends Activity {
 //        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 //        foregroundNote = builder.setCustomContentView(bigView).setSmallIcon(R.mipmap.ic_launcher).build();
 
-        Notification.Builder mNotifyBuilder = new Notification.Builder(this);
-        foregroundNote = mNotifyBuilder.setContent(bigView)
-                .setSmallIcon(R.mipmap.ic_launcher)
+        Notification.Builder notifyBuilder = new Notification.Builder(this);
+        foregroundNote = notifyBuilder.setContent(bigView)
+                .setSmallIcon(R.drawable.icon)
                 .build();
 
 //        foregroundNote.bigContentView = bigView;
@@ -38,5 +39,16 @@ public class MainActivity extends Activity {
 // now show notification..
         NotificationManager mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotifyManager.notify(1, foregroundNote);
+    }
+
+    private void showNotification() {
+        Notification notification;
+        Notification.Builder builder = new Notification.Builder(this);
+        notification = builder.setSmallIcon(R.drawable.icon)
+                .setContentTitle("发现新的自启应用")
+                .setContentText("招商银行 会在后台静默启动")
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(2, notification);
     }
 }
