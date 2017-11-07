52791adf8e77e73ba354f2f58be8d86e405dfac6
WolfXu <wolf.xu@ximalaya.com>
2017-11-07 18:35:20 +0800

    not finish code , but for sync

---
+++ databinding/src/main/java/com/gmail/huashadow/study/databinding/MyHandlers.java
@@ -0,0 +1,16 @@
+package com.gmail.huashadow.study.databinding;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * @author WolfXu on 2017/11/7.
+ */
+
+public class MyHandlers {
+    public void onClickFriend(View view) {
+        String name  = ((TextView) view).getText();
+        Toast.makeText((), "", Toast.LENGTH_SHORT).show();
+    }
+}
