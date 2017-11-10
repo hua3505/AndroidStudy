fd50546ca4c06360e1625cf64e8f21df4cfbd50c
WolfXu <wolf.xu@ximalaya.com>
2017-11-10 18:25:58 +0800

    study databinding: Listening Binding

---
+++ databinding/src/main/java/com/gmail/huashadow/study/databinding/ListeningBindingActivity.java
@@ -0,0 +1,26 @@
+package com.gmail.huashadow.study.databinding;
+
+import android.app.Activity;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gmail.huashadow.study.databinding.databinding.ActivityFirstDemoBinding;
+import com.gmail.huashadow.study.databinding.databinding.ActivityListeningBindingBinding;
+
+public class ListeningBindingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityListeningBindingBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_listening_binding);
+        binding.setPresenter(new Presenter());
+    }
+
+    public class Presenter {
+        public void onCompletedChanged(boolean completed) {
+
+        }
+    }
+}
